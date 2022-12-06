59ee130cd63d36b3a1fb87596786ba78c08d2c64
Rivzer <50300508+Rivzer@users.noreply.github.com>
2022-12-06 01:41:38 +0100

    1.1
    updated script

--- bash.sh
+++ bash.sh
@@ -1,14 +1,8 @@
 apt update -y;
-
 apt install docker.io;
-
-docker pull peer2profit/peer2profit_linux,
-
+docker pull peer2profit/peer2profit_linux;
+echo "Type your peer2profit email";
 read email;
-
 export P2P_EMAIL=$email; 
-
-docker rm -f peer2profit || true && docker run -d --restart always \
-        -e P2P_EMAIL=$P2P_EMAIL \
-        --name peer2profit \
-        peer2profit/peer2profit_linux:latest 
+docker rm -f peer2profit || true && docker run -d --restart always \ -e P2P_EMAIL=$P2P_EMAIL \ --name peer2profit \ peer2profit/peer2profit_linux:latest
+echo "Done! Thank you for using my script.";
