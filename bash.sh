40332b7c6ddafdc14fccdd0b55ced2762806aa55
Rivzer <50300508+Rivzer@users.noreply.github.com>
2022-12-06 02:03:33 +0100

    Add files via upload

--- bash.sh
+++ bash.sh
@@ -1,8 +1,12 @@
-apt update -y;
-apt install docker.io;
-docker pull peer2profit/peer2profit_linux;
-echo "Type your peer2profit email";
-read email;
-export P2P_EMAIL=$email; 
-docker rm -f peer2profit || true && docker run -d --restart always \ -e P2P_EMAIL=$P2P_EMAIL \ --name peer2profit \ peer2profit/peer2profit_linux:latest
-echo "Done! Thank you for using my script.";
+#!/bin/bash
+apt update -y
+apt install docker.io
+docker pull peer2profit/peer2profit_linux
+echo "Type your peer2profit email"
+read email
+export P2P_EMAIL=$email
+docker rm -f peer2profit || true && docker run -d --restart always \
+        -e P2P_EMAIL=$email \
+        --name peer2profit \
+        peer2profit/peer2profit_linux:latest 
+echo "Done! Thank you for using my script."
